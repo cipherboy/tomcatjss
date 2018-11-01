510cbddafdb1c99b0185f61e987d966c6643dad4
Alexander Scheel <ascheel@redhat.com>
2018-11-01 16:15:59 -0400

    Testing?
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- tomcat-8.5/src/org/apache/tomcat/util/net/jss/JSSImplementation.java
+++ tomcat-8.5/src/org/apache/tomcat/util/net/jss/JSSImplementation.java
@@ -13,34 +13,101 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * Copyright (C) 2018 Red Hat, Inc.
+ * Copyright (C) 2007 Red Hat, Inc.
  * All rights reserved.
  * END COPYRIGHT BLOCK */
 
 package org.apache.tomcat.util.net.jss;
 
-import javax.net.ssl.SSLSession;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Properties;
 
-import org.apache.tomcat.util.net.SSLHostConfigCertificate;
+import org.apache.tomcat.util.net.AbstractEndpoint;
 import org.apache.tomcat.util.net.SSLImplementation;
 import org.apache.tomcat.util.net.SSLSupport;
 import org.apache.tomcat.util.net.SSLUtil;
+import org.apache.tomcat.util.net.ServerSocketFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JSSImplementation extends SSLImplementation {
 
-    @Override
-    public SSLSupport getSSLSupport(SSLSession session) {
-        return new JSSSupport();
+    public static Logger logger = LoggerFactory.getLogger(JSSImplementation.class);
+
+    static final String JSSFactory = "org.apache.tomcat.util.net.jss.JSSFactory";
+    static final String SSLSocketClass = "org.mozilla.jss.ssl.SSLSocket";
+
+    private JSSFactory factory = null;
+
+    public JSSImplementation() throws ClassNotFoundException {
+        Class.forName(SSLSocketClass);
+
+        try {
+            Class<?> factcl = Class.forName(JSSFactory);
+            factory = (JSSFactory) factcl.newInstance();
+        } catch (Exception e) {
+            logger.error("Error getting factory: " + JSSFactory, e);
+        }
     }
 
-    @Override
-    public SSLUtil getSSLUtil(SSLHostConfigCertificate cert) {
-        return new JSSUtil();
+    public String getImplementationName() {
+        return "JSS";
     }
 
-    @Override
-    public boolean isAlpnSupported() {
-        return false;
+    public ServerSocketFactory getServerSocketFactory(AbstractEndpoint<?> endpoint) {
+
+        Properties config = new Properties();
+
+        try {
+            String configFile = System.getProperty("catalina.base") + "/conf/tomcatjss.conf";
+            config.load(new FileReader(configFile));
+        } catch (FileNotFoundException e) {
+            // ignore
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return factory.getSocketFactory(endpoint, config);
     }
 
+    public SSLSupport getSSLSupport(Socket s) {
+        SSLSupport ssls = null;
+        ssls = factory.getSSLSupport(s);
+        return ssls;
+    }
+
+    public SSLSupport getSSLSupport(javax.net.ssl.SSLSession session) {
+        /*
+         * The Tomcat 6.0.26 docs says: This method has been deprecated since it
+         * adds a JSSE dependency to this interface. It will be removed in
+         * versions after 6.0.x.
+         *
+         * But we have to provide a implementation of this method because it's
+         * declared as abstract.
+         *
+         * Unfortunately there does not appear to be any way to get SSLSupport
+         * information from a session with JSS. JSS looks up the information
+         * based on a socket, not a session. This done in SSLSocket.c
+         * Java_org_mozilla_jss_ssl_SSLSocket_getStatus().
+         *
+         * So while it would be nice to provide a working implmentation there
+         * doesn't seem to be an easy way to do this. Given that this method is
+         * already deprecated and there hasn't been any evidence of it being
+         * called it therefore seems reasonable to just return null to satify
+         * the compiler's demand for an implementation.
+         *
+         * Once this abstract method is removed from SSLImplementation in a
+         * future release we can remove this stub.
+         *
+         * NOTE: This method has NOT yet been deprecated in Tomcat 7!
+         */
+        return null;
+    }
+
+    public SSLUtil getSSLUtil(AbstractEndpoint<?> endpoint) {
+        return null;
+    }
 }
--- tomcat-8.5/src/org/apache/tomcat/util/net/jss/JSSSupport.java
+++ tomcat-8.5/src/org/apache/tomcat/util/net/jss/JSSSupport.java
@@ -13,42 +13,93 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
- * Copyright (C) 2018 Red Hat, Inc.
+ * Copyright (C) 2007 Red Hat, Inc.
  * All rights reserved.
  * END COPYRIGHT BLOCK */
 
 package org.apache.tomcat.util.net.jss;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
 import org.apache.tomcat.util.net.SSLSupport;
+import org.mozilla.jss.ssl.SSLSecurityStatus;
+import org.mozilla.jss.ssl.SSLSocket;
 
-public class JSSSupport implements SSLSupport {
+class JSSSupport implements SSLSupport {
 
-    @Override
+    private SSLSocket ssl = null;
+    private SSLSecurityStatus status = null;
+
+    JSSSupport(SSLSocket sock) {
+        ssl = sock;
+        try {
+            status = ssl.getStatus();
+        } catch (IOException e) {
+        }
+    }
+
+    public X509Certificate[] getPeerCertificateChain(boolean force)
+            throws IOException {
+        // retrieve the status when we need it. status cache
+        // the client certificate which may not be available
+        // at the creation of JSSSupport
+        status = ssl.getStatus();
+        if (status != null) {
+            org.mozilla.jss.crypto.X509Certificate peerCert = status
+                    .getPeerCertificate();
+
+            if (peerCert == null) {
+                ssl.requireClientAuth(SSLSocket.SSL_REQUIRE_NO_ERROR);
+                try {
+                    ssl.redoHandshake();
+                    ssl.forceHandshake();
+                } catch (Exception e) {
+                }
+                status = ssl.getStatus();
+                peerCert = status.getPeerCertificate();
+            }
+
+            if (peerCert != null) {
+                X509Certificate[] certs = new X509Certificate[1];
+                try {
+                    byte[] b = peerCert.getEncoded();
+                    CertificateFactory cf = CertificateFactory
+                            .getInstance("X.509");
+                    ByteArrayInputStream stream = new ByteArrayInputStream(b);
+                    certs[0] = (X509Certificate) cf.generateCertificate(stream);
+                } catch (Exception e) {
+                }
+                return certs;
+            }
+        }
+
+        return null;
+    }
+
+    public Object[] getPeerCertificateChain() throws IOException {
+        return getPeerCertificateChain(false);
+    }
+
     public String getCipherSuite() throws IOException {
+        if (status != null)
+            return status.getCipher();
         return null;
     }
 
-    @Override
     public Integer getKeySize() throws IOException {
+        if (status != null)
+            return (new Integer(status.getSessionKeySize()));
         return null;
     }
 
-    @Override
-    public X509Certificate[] getPeerCertificateChain() throws IOException {
-        return null;
-    }
-
-    @Override
     public String getProtocol() throws IOException {
         return null;
     }
 
-    @Override
     public String getSessionId() throws IOException {
         return null;
     }
-
- }
+}
